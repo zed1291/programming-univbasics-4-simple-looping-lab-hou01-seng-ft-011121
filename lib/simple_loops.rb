c8f1504639501241a44f438f4a60ac32fa578d1b
zed <zed@kolk.me>
2020-12-14 23:41:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,3 +13,11 @@ def loop_message_n_times message, n
     counter += 1
   end
 end
+
+def output_array message, n
+  counter = 0
+  while counter < n do
+    puts message
+    counter += 1
+  end
+end
