f11e8b0e002b7849717ef8c7a9bf1970f72f0aa9
zed <zed@kolk.me>
2020-12-14 23:40:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,11 @@ def loop_message_five_times message
     counter += 1
   end
 end
+
+def loop_message_n_times message, n
+  counter = 0
+  while counter < n do
+    puts message
+    counter += 1
+  end
+end
